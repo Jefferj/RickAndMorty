b98f35f84ea657881df6202a6a072f88583c9622
Jefferj <jefferj080@gmail.com>
2022-09-30 15:39:06 -0500

    First commit

--- RickAndMorty/App/Scenes/RickDetails/RickListView/RickListViewController.swift
+++ RickAndMorty/App/Scenes/RickDetails/RickListView/RickListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RickListViewController.swift
 //  RickAndMorty
 //
 //  Created by Jefferson Naranjo rodríguez on 30/09/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RickListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .lightGray
     }
 
-
 }
-
---
+++ RickAndMorty/App/Scenes/RickList/RickListView/RickDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RickDetailViewController.swift
+//  RickAndMorty
+//
+//  Created by Jefferson Naranjo rodríguez on 30/09/22.
+//
+
+import Foundation
+import UIKit
+
+class RickDetailViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .lightGray
+    }
+}
